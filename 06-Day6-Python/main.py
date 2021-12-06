648fb8c9a79a264a88057f193be32d0ea15f3c97
Janek <janek.graff@gmx.de>
2021-12-06 16:15:06 +0100

    update to not use any dependency

--- 06-Day6-Python/main.py
+++ 06-Day6-Python/main.py
@@ -1,28 +1,31 @@
-import numpy as np
-from collections import Counter
-
-
 def main(input_file: str):
     with open(input_file, 'r') as file:
         input = file.read()
-        fishes = np.array([int(i) for i in input.split(',')])
+        fishes = [int(i) for i in input.split(',')]
 
     print('Part 1: There are a total of {} fishes'.format(
-        solution(fishes, days=80)))
+        solution2(fishes, days=80)))
     print('Part 2: There are a total of {} fishes'.format(
-        solution(fishes, days=256)))
+        solution2(fishes, days=256)))
 
 
-def solution(fishes: np.ndarray, days: int) -> int:
-    fish_count = Counter(fishes)
+def solution2(fishes: list[int], days: int) -> int:
+    fish_counter: list[int] = [0 for i in range(9)]
+    for i in fishes:
+        fish_counter[i] += 1
+
     for day in range(days):
-        tmp = [fish_count[i] for i in range(9)]
-        fish_count[8] = fish_count[0]
-        tmp[7] += fish_count[0]
-        for i, x in list(enumerate(tmp))[1:]:
-            fish_count[i-1] = x
+        tmp: list[int] = [fish_counter[i] for i in range(len(fish_counter))]
+        fish_counter[8] = fish_counter[0]
+        tmp[7] += fish_counter[0]
+        for idx, x in list(enumerate(tmp))[1:]:
+            fish_counter[idx - 1] = x
 
-    return fish_count.total()
+    total: int = 0
+    for i in fish_counter:
+        total += i
+
+    return total
 
 
 if __name__ == '__main__':
