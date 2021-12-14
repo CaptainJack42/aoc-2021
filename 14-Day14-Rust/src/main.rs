cad68ab7c2e26df2791d07870578ea6e15bd1fa5
Janek <janek.graff@gmx.de>
2021-12-14 13:53:01 +0100

    day 14 part2

--- 14-Day14-Rust/src/main.rs
+++ 14-Day14-Rust/src/main.rs
@@ -58,25 +58,29 @@ fn part_1(steps: isize, mut template: Vec<String>, lookup: &HashMap<String, Stri
 
 fn part_2(steps: isize, template: Vec<String>, lookup: &HashMap<String, String>) -> u64 {
     let mut counter: HashMap<String, u64> = HashMap::new();
-    let start_i = template.windows(2).collect::<Vec<_>>();
-    // FIXME: Better but still fails at step 23. strlen is 2^(n+1)+1 -> needs A LOT of memory
-    for n in start_i {
-        let mut curr: Vec<String> = vec![n[0].clone(), n[1].clone()];
-        *counter.entry(n[0].clone()).or_insert(0) += 1;
-        *counter.entry(n[1].clone()).or_insert(0) += 1;
-        for _i in 0..steps {
-            let mut next_step: Vec<String> = Vec::new();
-            let tmp_i = curr.windows(2).collect::<Vec<_>>();
-            next_step.push(curr[0].clone());
-            for m in tmp_i {
-                let key: String = String::new() + &m[0] + &m[1];
-                let val = lookup.get(&key).unwrap().to_string();
-                next_step.push(val.clone());
-                *counter.entry(val).or_insert(0) += 1;
-                next_step.push(m[1].clone());
+    let mut pair_counter: HashMap<String, u64> = HashMap::new();
+    for n in &template {
+        *counter.entry(n.clone()).or_insert(0) += 1;
+    }
+    for n in template.windows(2).collect::<Vec<_>>() {
+        let key: String = String::new() + &n[0] + &n[1];
+        *pair_counter.entry(key).or_insert(0) += 1;
+    }
+    for _i in 0..steps {
+        let mut new: Vec<(String, String, u64)> = Vec::new();
+        for (key, val) in lookup {
+            if pair_counter.contains_key(key){
+                new.push((key.clone(), val.clone(), *pair_counter.get(key).unwrap()));
+                pair_counter.remove(key);
             }
-            curr = next_step.clone();
-            println!("steps: {}; len: {}", _i, curr.len());
+        }
+        for (pair, c, cnt) in new {
+            *counter.entry(c.clone()).or_insert(0) += cnt;
+            let pair = pair.chars().collect::<Vec<_>>();
+            let p1 = String::new() + &pair[0].to_string() + &c;
+            let p2 = String::new() + &c + &pair[1].to_string();
+            *pair_counter.entry(p1).or_insert(0) += cnt;
+            *pair_counter.entry(p2).or_insert(0) += cnt; 
         }
     }
     let max = counter.iter().max_by_key(|entry| entry.1).unwrap().1;
